ed3b1dfbdb8bc346ae6c7890f4f49a5db3959751
emmettwen <15708200+emmettwen@user.noreply.gitee.com>
2025-09-17 13:46:34 +0800

    update

--- src/views/orders/Index.vue
+++ src/views/orders/Index.vue
@@ -20,20 +20,24 @@
     loadItems,
   } = useTableServer<Order>('order', ['product', 'paymentPic', 'customer'])
   const headers = computed(() => [
-    { title: t('order.orderNumber'), key: 'documentId' },
-    { title: t('order.product'), key: 'product.name' },
-    { title: t('order.customer'), key: 'customer.username' },
-    { title: t('order.status'), key: 'orderStatus' },
-    { title: t('order.machineCode'), key: 'code' },
-    { title: t('product.qty'), key: 'qty' },
-    { title: t('checkout.total'), key: 'total' },
-    { title: t('product.duration'), key: 'duration' },
-    { title: '收益', key: 'group' },
+    { title: t('order.orderNumber'), key: 'documentId', sortable: false },
+    { title: t('order.product'), key: 'product.name', sortable: false },
+    { title: t('order.customer'), key: 'customer.username', sortable: false },
+    { title: t('order.status'), key: 'orderStatus', sortable: false },
+    { title: t('order.machineCode'), key: 'code', sortable: false },
+    { title: 'Broker Name', key: 'brokerName', sortable: false },
+    { title: 'Broker Server', key: 'brokerServer', sortable: false },
+    { title: 'Transaction Account', key: 'transactionAccount', sortable: false },
+    { title: t('product.qty'), key: 'qty', sortable: false },
+    { title: t('checkout.total'), key: 'total', sortable: false },
+    { title: t('product.duration'), key: 'duration', sortable: false },
+    { title: '收益', key: 'group', sortable: false },
     { title: t('product.actions'), key: 'actions', align: 'end' },
   ] as const)
 
   const dialog = ref(false)
   const groupDialog = ref(false)
+  const serverDialog = ref(false)
   const groupItems = ref([
     { title: '4%', value: '00' },
     { title: '5%', value: '01' },
@@ -42,6 +46,10 @@
   ])
   const groupId = ref('00')
   const target = ref('')
+  const serverTarget = ref()
+  const name = ref()
+  const server = ref()
+  const account = ref()
 
   const chooseRate = (documentId: string) => {
     target.value = documentId
@@ -61,6 +69,29 @@
         groupDialog.value = false
       })
     }
+  }
+  const onClickServerInformation = (item) => {
+    serverTarget.value = item
+    name.value = item.brokerName
+    server.value = item.brokerServer
+    account.value = item.transactionAccount
+    serverDialog.value = true
+  }
+  const updateServerInformation = () => {
+    request({
+      ...api.order.update(serverTarget.value.documentId),
+      data: {
+        data: {
+          brokerName: name.value,
+          brokerServer: server.value,
+          transactionAccount: account.value,
+        },
+      },
+    }).then(res => {
+      message.success('更新成功！')
+      serverDialog.value = false
+      loadItems()
+    })
   }
 
   const confirmationTarget = ref<Order>(null)
@@ -127,12 +158,34 @@
         {{ value ? value.name : '' }}
       </template>
       <template #[`item.actions`]="{item}">
-        <v-btn
-          icon="mdi-eye"
-          size="small"
-          variant="text"
-          @click="showImage(item)"
-        />
+        <v-tooltip interactive location="top">
+          <template #activator="{ props: activatorProps }">
+            <v-btn
+              v-bind="activatorProps"
+              icon="mdi-eye"
+              size="small"
+              variant="text"
+              @click="showImage(item)"
+            />
+          </template>
+          <div>
+            支付凭证
+          </div>
+        </v-tooltip>
+        <v-tooltip interactive location="top">
+          <template #activator="{ props: activatorProps }">
+            <v-btn
+              v-bind="activatorProps"
+              icon="mdi-server-network"
+              size="small"
+              variant="text"
+              @click="onClickServerInformation(item)"
+            />
+          </template>
+          <div>
+            绑定信息
+          </div>
+        </v-tooltip>
         <v-btn
           v-if="item.orderStatus === 'pending'"
           icon="mdi-check"
@@ -175,6 +228,38 @@
           <v-spacer />
           <v-btn @click="groupDialog = false">{{ t('cancel') }}</v-btn>
           <v-btn @click="approveOrder">{{ t('submit') }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="serverDialog"
+      persistent
+      width="500px"
+    >
+      <v-card>
+        <v-card-title>已绑定信息</v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="name"
+              density="compact"
+              label="Broker Name"
+            ></v-text-field>
+            <v-text-field
+              v-model="server"
+              density="compact"
+              label="Broker Server"
+            ></v-text-field>
+            <v-text-field
+              v-model="account"
+              density="compact"
+              label="Transaction Account"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="serverDialog = false">{{ t('close') }}</v-btn>
+          <v-btn color="primary" @click="updateServerInformation">{{ t('save') }}</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
